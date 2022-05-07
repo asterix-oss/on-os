3a7857616877294f9246869c38933b453c6403f6
Ope <opeabidemi@gmail.com>
2022-05-07 22:06:28 +0100

    added husky, eslint, prettier and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    // Prettier
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+      },
+    },
+  ],
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+  },
+};
