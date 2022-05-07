de60fb478a462a78cae9a6ba861103fed58ecd37
Ope <opeabidemi@gmail.com>
2022-05-07 21:51:56 +0100

    added tailwind and sass support

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#090909",
+        primary: "#ED4343"
+      }
+    },
+  },
+  plugins: [],
+}
