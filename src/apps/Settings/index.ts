6024be47227c7b6b93cdc690160fccc9a00521a1
Ope <opeabidemi@gmail.com>
2022-05-31 11:39:51 +0100

    feat: added Settings App

--- src/apps/Settings/index.ts
+++ src/apps/Settings/index.ts
@@ -3,7 +3,7 @@ import Home from "./Home";
 
 export const Settings: App = {
   name: "Settings",
-  icon: "https://images.unsplash.com/photo-1611262588024-d12430b98920?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aWNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500",
+  icon: require("./assets/logo.png"),
   description: "User Preferences",
   module: Home,
   isFullScreen: false,
